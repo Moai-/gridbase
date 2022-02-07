a765d2ec8b8008e64b393de39f59336bbb3d9c4f
Sergei Gmyria <sv.gmyria@gmail.com>
2022-02-06 18:42:04 -0600

    added movement

--- src/rules/common/theme/rectanglePiece.tsx
+++ src/rules/common/theme/rectanglePiece.tsx
@@ -1,6 +1,15 @@
 import { GridEntity } from '../../../engine/defaultTheme'
 import { EntityRenderer } from '../../../engine/theme'
 import { Rectangle } from '../rectangle'
+import { useDrop } from 'react-dnd'
+import styled from 'styled-components'
+
+const DropDiv = styled.div`
+    display: block;
+    width: 100%;
+    height: 100%;
+    position: relative;
+`
 
 export const RectanglePiece: EntityRenderer = (props) => {
     const { entity } = props
@@ -16,7 +25,27 @@ export const RectanglePiece: EntityRenderer = (props) => {
     if ( transparent ) {
         style.opacity = 0.3
     }
+
+    const [{ isOver }, drop] = useDrop(
+        () => ({
+          accept: 'character',
+          drop: () => entity.drop(),
+          collect: (monitor) => ({
+            isOver: !!monitor.isOver()
+          })
+        }),
+        []
+    )
+
+    const handleClick = () => {
+        
+    }
+
     return (
-        <GridEntity {...props} style={style} />
+        <GridEntity {...props} style={style}>
+            <DropDiv ref={drop}>
+                {props.children}
+            </DropDiv>
+        </GridEntity>
     )
 }
